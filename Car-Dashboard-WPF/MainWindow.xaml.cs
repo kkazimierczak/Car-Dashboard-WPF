02153d135d068b6f86ef3f86c4aadcc1aea49ec7
kkazimierczak <kszysopl@gmail.com>
2017-05-29 20:23:07 +0200

    Minor fixes

--- Car-Dashboard-WPF/MainWindow.xaml.cs
+++ Car-Dashboard-WPF/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Diagnostics;
 using System.Threading;
 using System.Windows;
 using System.Windows.Threading;
+using Microsoft.Research.DynamicDataDisplay;
+using Microsoft.Research.DynamicDataDisplay.DataSources;
+using System.Collections.ObjectModel;
 
 namespace Car_Dashboard_WPF
 {
@@ -18,12 +21,24 @@ namespace Car_Dashboard_WPF
     {
         EngineModel engine;
         delegate void UpdaterDelegate();
+        public ObservableCollection<Point> Data { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
             Top = 0;
-            Left = 50;
+            Left = -5;
+
+            ObservableDataSource<Point> data = new ObservableDataSource<Point>();
+            LineGraph linegraph = new LineGraph(data);
+
+            var p1 = new Point(0, 1);
+            var p2 = new Point(4, 4);
+
+            data.AppendAsync(Dispatcher, p1);
+            data.AppendAsync(Dispatcher, p2);
+
+            //Plot.Children.Add(linegraph);
 
             engine = new EngineModel();
             Loaded += MainWindow_Loaded;
@@ -61,7 +76,7 @@ namespace Car_Dashboard_WPF
                 }));
                 FuelUsageTextBox.Dispatcher.Invoke(new UpdaterDelegate(() =>
                 {
-                    FuelUsageTextBox.Text = Math.Round(engine.fuelUsage * 3600, 1).ToString();
+                    FuelUsageTextBox.Text = Math.Round(engine.fuelUsage * 3600, 1).ToString() + " l/100 km";
                 }));
                 FuelGauge.Dispatcher.Invoke(new UpdaterDelegate(() =>
                 {
